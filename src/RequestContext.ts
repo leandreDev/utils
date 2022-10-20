0528a11a72dce73a0ff46bc250c5d7cdfeb59150
Benoît Boutigny <b2devpro@gmail.com>
2022-10-20 11:21:14 +0200

    style: clean style & add interface for RequestContext class

--- src/RequestContext.ts
+++ src/RequestContext.ts
@@ -1,11 +1,13 @@
 export class RequestContext {
-  'null' = null;
-  'emptyStr' = '';
+  public null: null = null;
+
+  public emptyStr: string = '';
+
   get now(): number {
     return Date.now();
   }
 
-  get DateNow() {
+  get DateNow(): Date {
     return new Date();
   }
 }
