4e43eba188222d4ac156d4e38f42f722083a9ab0
Benoît Boutigny <b2devpro@gmail.com>
2022-10-21 10:28:15 +0200

    chore: add interface & format code

--- src/ConfLoader.ts
+++ src/ConfLoader.ts
@@ -1,32 +1,35 @@
+import * as fs from 'fs-extra';
+import * as assert from 'assert';
 import * as request from 'request-promise-native';
+
 import { CtxInterpretor } from './CtxInterpretor';
 import { UtilsSecu } from './UtilsSecu';
-import * as fs from 'fs-extra';
-
-import * as assert from 'assert';
 
 export class ConfLoader {
+  constructor() {}
+
   static getConf(): Promise<any> {
     return new Promise((resolve, reject) => {
       const options: any = {};
 
-      assert(process.env.CONF_URL, '$env.CONF_URL is not spécified');
-      assert(process.env.SRV_ID, '$env.SRV_ID is not spécified');
-      assert(process.env.SECRET, '$env.SECRET is not spécified');
+      assert(process.env.CONF_URL, '$env.CONF_URL is not specified');
+      assert(process.env.SRV_ID, '$env.SRV_ID is not specified');
+      assert(process.env.SECRET, '$env.SECRET is not specified');
+
       options.url = process.env.CONF_URL + process.env.SRV_ID;
       options.json = true;
+
       const secu: UtilsSecu = new UtilsSecu({
         conf: { secretKey: process.env.SECRET, debug: false },
       });
-      const contextInterpretor: CtxInterpretor = new CtxInterpretor(
-        process.env
-      );
+
+      const contextInterpretor: CtxInterpretor = new CtxInterpretor(process.env);
+
       if (process.env.CONF_URL == 'none') {
         try {
-          const val = fs.readJSONSync(
-            './confs/' + process.env.SRV_ID + '.json'
-          );
+          const val = fs.readJSONSync('./confs/' + process.env.SRV_ID + '.json');
           const conf: any = contextInterpretor.updateEnv(val);
+
           resolve(val);
         } catch (err) {
           console.info('offline confloader error read JSON', err);
@@ -34,8 +37,10 @@ export class ConfLoader {
         }
       } else {
         let tempConf;
+
         if (fs.existsSync('./confs/' + process.env.SRV_ID + '.json')) {
           tempConf = fs.readJSONSync('./confs/' + process.env.SRV_ID + '.json');
+
           if (tempConf.loadConfAfter) {
             ConfLoader.loadConf().catch((err) => {
               console.error(err);
@@ -53,50 +58,55 @@ export class ConfLoader {
 
   static loadConf(): Promise<any> {
     const options: any = {};
+
     options.url = process.env.CONF_URL + process.env.SRV_ID;
     options.json = true;
+
     const secu: UtilsSecu = new UtilsSecu({
       conf: { secretKey: process.env.SECRET, debug: false },
     });
+
     const contextInterpretor: CtxInterpretor = new CtxInterpretor(process.env);
+
     secu.addHeadersKey(options);
+
     return Promise.resolve(
       request
         .get(options)
         .then((val) => {
           let data: any;
+
           if (val && val.code == 200 && val.response && val.response[0]) {
             data = val.response[0];
+
             fs.ensureDirSync('./confs');
             fs.writeJSONSync('./confs/' + process.env.SRV_ID + '.json', data, {
               spaces: 2,
             });
           } else {
             if (val && val.code != 200) {
-              console.error(
-                'online confloader error read JSON',
-                val,
-                options.url
-              );
+              console.error('online confloader error read JSON', val, options.url);
             }
             data = fs.readJSONSync('./confs/' + process.env.SRV_ID + '.json');
           }
+
           const conf: any = contextInterpretor.updateEnv(data);
+
           return data;
         })
         .catch((err) => {
           try {
             console.error('confloader error on JSON ', err);
-            const val = fs.readJSONSync(
-              './confs/' + process.env.SRV_ID + '.json'
-            );
+
+            const val = fs.readJSONSync('./confs/' + process.env.SRV_ID + '.json');
             const conf: any = contextInterpretor.updateEnv(val);
+
             return val;
           } catch (err2) {
             console.error('confloader fatal error ', err2);
             throw err;
           }
-        })
+        }),
     );
   }
 }
