e9fd0558a780b250e827917e562d197d99027a0a
Benoît Boutigny <b2devpro@gmail.com>
2022-11-03 09:10:02 +0100

    test: add UtilsSecu tests

--- test/UtilsSecu.spec.ts
+++ test/UtilsSecu.spec.ts
@@ -1,28 +1,142 @@
 import { describe, expect, it } from '@jest/globals';
-import { createRequest } from 'node-mocks-http';
+import { createRequest, RequestOptions, createResponse } from 'node-mocks-http';
 
 import { IApplicationConfiguration } from '../src/IApplicationConfiguration';
-import { UtilsSecu } from '../src/UtilsSecu';
+import { UtilsSecu, ICtxRequest } from '../src/UtilsSecu';
 
 const appConf: IApplicationConfiguration = {
-  conf: { secretKey: 'poipoi', debug: true },
+  conf: { secretKey: 'poipoi', debug: false, urlBase: 'http://localhost:3114' },
 };
 
-const request = createRequest({
-  method: 'GET',
-  url: '/this/is/some/random/url',
-  headers: {
-    ['keyDate']: Date.now().toString(),
-  },
-});
+const next = (msg) => {
+  console.log(msg);
+};
 
 describe('UtilsSecu', () => {
-  it('should have a valid application configuration', () => {
-    expect(new UtilsSecu(appConf));
+  describe('constructor', () => {
+    it('should have a valid application configuration', () => {
+      expect(new UtilsSecu(appConf));
+    });
   });
 
-  it('test', () => {
-    new UtilsSecu(appConf).addHeadersKey(request);
-    expect(request);
+  describe('protectInternalMidelWare', () => {
+    it('should throw and error if key is not present', () => {
+      const requestOptions: RequestOptions = {
+        method: 'GET',
+        url: '/this/is/some/random/url',
+        headers: {
+          // key: 'poipoi'
+          // keyDate:(Date.now() - 5000).toString(),
+        },
+      };
+
+      const request = createRequest(requestOptions);
+      const response = createResponse();
+      const utilSecu = new UtilsSecu(appConf);
+
+      expect(() => {
+        utilSecu.protectInternalMidelWare(request, response, next);
+      }).toThrow(new Error('no key'));
+    });
+
+    /*
+
+    it('should throw and error if keydate is not present', () => {
+      const requestOptions: RequestOptions = {
+        method: 'GET',
+        url: '/this/is/some/random/url',
+        headers: {
+          key: 'poipoi',
+          // keyDate:(Date.now() - 5000).toString(),
+        },
+      };
+
+      const request = createRequest(requestOptions);
+      const response = createResponse();
+      const utilSecu = new UtilsSecu(appConf);
+
+      expect(() => {
+        utilSecu.protectInternalMidelWare(request, response, next);
+      }).toThrow(new Error('____'));
+    });
+    */
+
+    it('should throw and error if keydate is obselete', () => {
+      const requestOptions: RequestOptions = {
+        method: 'GET',
+        url: '/this/is/some/random/url',
+        headers: {
+          key: 'xxxxxxxxx',
+          keyDate: (Date.now() - (30000 + 10)).toString(),
+        },
+      };
+
+      const request = createRequest(requestOptions);
+      const response = createResponse();
+      const utilSecu = new UtilsSecu(appConf);
+
+      expect(() => {
+        utilSecu.protectInternalMidelWare(request, response, next);
+      }).toThrow(new Error('keyDate is obsolete'));
+    });
+
+    it('should throw and error if key is note valid', () => {
+      const requestOptions: RequestOptions = {
+        method: 'GET',
+        url: '/this/is/some/random/url',
+        headers: {
+          key: 'poipoi',
+          keyDate: (Date.now() - (30000 + 10)).toString(),
+        },
+      };
+
+      const request = createRequest(requestOptions);
+      const response = createResponse();
+      const utilSecu = new UtilsSecu(appConf);
+
+      expect(() => {
+        utilSecu.protectInternalMidelWare(request, response, next);
+      }).toThrow(new Error('keyDate is obsolete'));
+    });
+
+    it('should set request.ctx.internalCallValid to true for a valid key', () => {
+      const requestOptions: RequestOptions = {
+        method: 'GET',
+        url: '/this/is/some/random/url',
+        headers: {
+          key: 'xxxxxx',
+          keyDate: (Date.now() - (30000 + 10)).toString(),
+        },
+      };
+
+      const request = createRequest(requestOptions);
+      const response = createResponse();
+      const utilSecu = new UtilsSecu(appConf);
+
+      expect(() => {
+        utilSecu.protectInternalMidelWare(request, response, next);
+      }).toThrow(new Error('keyDate is obsolete'));
+    });
+
+    it('should set request.ctx.internalCallValid to true for a valid key', () => {
+      const requestOptions: RequestOptions = {
+        method: 'GET',
+        url: '',
+        headers: {
+          key: '8e6215ce4784784bc99881ab13cb64fd1dc49fbfe2fdb76fb0007cd2c90eadae',
+          keyDate: Date.now().toString(),
+        },
+      };
+
+      const request = createRequest(requestOptions);
+      const response = createResponse();
+      const utilSecu = new UtilsSecu(appConf);
+
+      const next = () => {
+        expect(request.ctx.internalCallValid).toBe(true);
+      };
+
+      utilSecu.protectInternalMidelWare(request, response, next);
+    });
   });
 });
