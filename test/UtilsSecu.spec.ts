a9d4eb33f16848e14357a993a0ec4a1011092cd3
Benoît Boutigny <b2devpro@gmail.com>
2022-11-03 10:50:04 +0100

    test: add test for UtilsSecu

--- test/UtilsSecu.spec.ts
+++ test/UtilsSecu.spec.ts
@@ -2,10 +2,10 @@ import { describe, expect, it } from '@jest/globals';
 import { createRequest, RequestOptions, createResponse } from 'node-mocks-http';
 
 import { IApplicationConfiguration } from '../src/IApplicationConfiguration';
-import { UtilsSecu, ICtxRequest } from '../src/UtilsSecu';
+import { UtilsSecu, ICtxRequest, IValidContext } from '../src/UtilsSecu';
 
 const appConf: IApplicationConfiguration = {
-  conf: { secretKey: 'poipoi', debug: false, urlBase: 'http://localhost:3114' },
+  conf: { secretKey: 'poipoi', debug: false, urlBase: 'http://localhost:3114/' },
 };
 
 const next = (msg) => {
@@ -40,7 +40,7 @@ describe('UtilsSecu', () => {
     });
 
     /*
-
+    keydate header is not tested fpr existence
     it('should throw and error if keydate is not present', () => {
       const requestOptions: RequestOptions = {
         method: 'GET',
@@ -61,7 +61,7 @@ describe('UtilsSecu', () => {
     });
     */
 
-    it('should throw and error if keydate is obselete', () => {
+    it('should throw and error if keydate is due (>30000ms)', () => {
       const requestOptions: RequestOptions = {
         method: 'GET',
         url: '/this/is/some/random/url',
@@ -85,8 +85,8 @@ describe('UtilsSecu', () => {
         method: 'GET',
         url: '/this/is/some/random/url',
         headers: {
-          key: 'poipoi',
-          keyDate: (Date.now() - (30000 + 10)).toString(),
+          key: 'xxxxxxxxx',
+          keyDate: (Date.now() - 0).toString(),
         },
       };
 
@@ -96,7 +96,7 @@ describe('UtilsSecu', () => {
 
       expect(() => {
         utilSecu.protectInternalMidelWare(request, response, next);
-      }).toThrow(new Error('keyDate is obsolete'));
+      }).toThrow(new Error('key dont match uri'));
     });
 
     it('should set request.ctx.internalCallValid to true for a valid key', () => {
@@ -104,39 +104,111 @@ describe('UtilsSecu', () => {
         method: 'GET',
         url: '/this/is/some/random/url',
         headers: {
-          key: 'xxxxxx',
-          keyDate: (Date.now() - (30000 + 10)).toString(),
+          key: '',
+          keyDate: (Date.now() - 1000).toString(),
         },
       };
 
       const request = createRequest(requestOptions);
       const response = createResponse();
       const utilSecu = new UtilsSecu(appConf);
-
-      expect(() => {
-        utilSecu.protectInternalMidelWare(request, response, next);
-      }).toThrow(new Error('keyDate is obsolete'));
-    });
-
-    it('should set request.ctx.internalCallValid to true for a valid key', () => {
-      const requestOptions: RequestOptions = {
-        method: 'GET',
-        url: '',
-        headers: {
-          key: '8e6215ce4784784bc99881ab13cb64fd1dc49fbfe2fdb76fb0007cd2c90eadae',
-          keyDate: Date.now().toString(),
-        },
-      };
-
-      const request = createRequest(requestOptions);
-      const response = createResponse();
-      const utilSecu = new UtilsSecu(appConf);
-
+      // create a valid sercurity header
+      utilSecu.addHeadersKey(request);
       const next = () => {
         expect(request.ctx.internalCallValid).toBe(true);
       };
 
       utilSecu.protectInternalMidelWare(request, response, next);
+    });
+  });
+
+  describe('chekInternalMidelWare', () => {
+    it('should throw and error if key is not present', () => {
+      const requestOptions: RequestOptions = {
+        method: 'GET',
+        url: '/this/is/some/random/url',
+        headers: {
+          // key: 'poipoi'
+          // keyDate:(Date.now() - 5000).toString(),
+        },
+      };
+
+      const request: ICtxRequest<IValidContext> = createRequest(requestOptions);
+      const response = createResponse();
+      const utilSecu = new UtilsSecu(appConf);
+      request.ctx = {};
+
+      const next = () => {
+        expect(true);
+      };
+      utilSecu.chekInternalMidelWare(request, response, next);
+    });
+
+    it('should throw and error if keydate is due (>30000ms)', () => {
+      const requestOptions: RequestOptions = {
+        method: 'GET',
+        url: '/this/is/some/random/url',
+        headers: {
+          key: 'xxxxxxxxx',
+          keyDate: (Date.now() - (30000 + 10)).toString(),
+        },
+      };
+
+      const request: ICtxRequest<IValidContext> = createRequest(requestOptions);
+      const response = createResponse();
+      const utilSecu = new UtilsSecu(appConf);
+      request.ctx = {};
+
+      const next = () => {
+        expect(request.ctx.internalCallValid).toBe(false);
+      };
+
+      utilSecu.chekInternalMidelWare(request, response, next);
+    });
+
+    it('should throw and error if key is note valid', () => {
+      const requestOptions: RequestOptions = {
+        method: 'GET',
+        url: '/this/is/some/random/url',
+        headers: {
+          key: 'xxxxxxxxx',
+          keyDate: (Date.now() - 0).toString(),
+        },
+      };
+
+      const request: ICtxRequest<IValidContext> = createRequest(requestOptions);
+      const response = createResponse();
+      const utilSecu = new UtilsSecu(appConf);
+      request.ctx = {};
+
+      const next = () => {
+        expect(request.ctx.internalCallValid).toBe(false);
+      };
+
+      utilSecu.chekInternalMidelWare(request, response, next);
+    });
+
+    it('should set request.ctx.internalCallValid to true for a valid key', () => {
+      const requestOptions: RequestOptions = {
+        method: 'GET',
+        url: '/this/is/some/random/url',
+        headers: {
+          key: '',
+          keyDate: (Date.now() - 1000).toString(),
+        },
+      };
+      const request: ICtxRequest<IValidContext> = createRequest(requestOptions);
+      const response = createResponse();
+      const utilSecu = new UtilsSecu(appConf);
+      utilSecu.addHeadersKey(request);
+
+      request.ctx = {};
+
+      const next = () => {
+        expect(request.ctx.internalCallValid).toBe(true);
+      };
+
+      utilSecu.chekInternalMidelWare(request, response, next);
     });
   });
 });
