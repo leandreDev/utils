29cd4e158f23626baaaa04bb55b555c06db181aa
Benoît Boutigny <b2devpro@gmail.com>
2022-11-04 08:15:27 +0100

    style: change object assignment & addd comment

--- src/ConfLoader.ts
+++ src/ConfLoader.ts
@@ -1,3 +1,5 @@
+import * as process from 'node:process';
+
 import * as fs from 'fs-extra';
 import * as assert from 'assert';
 import * as request from 'request-promise-native';
@@ -10,24 +12,31 @@ export class ConfLoader {
 
   static getConf(): Promise<any> {
     return new Promise((resolve, reject) => {
-      const options: any = {};
-
       assert(process.env.CONF_URL, '$env.CONF_URL is not specified');
       assert(process.env.SRV_ID, '$env.SRV_ID is not specified');
       assert(process.env.SECRET, '$env.SECRET is not specified');
 
-      options.url = process.env.CONF_URL + process.env.SRV_ID;
-      options.json = true;
+      /*
+        options is never user
+      */
+      const options: request.options = {
+        url: process.env.CONF_URL + process.env.SRV_ID,
+        json: true,
+      };
 
+      /*
+        secu is never used
+      */
       const secu: UtilsSecu = new UtilsSecu({
         conf: { secretKey: process.env.SECRET, debug: false },
       });
 
       const contextInterpretor: CtxInterpretor = new CtxInterpretor(process.env);
+      const filePath = './confs/' + process.env.SRV_ID + '.json';
 
       if (process.env.CONF_URL == 'none') {
         try {
-          const val = fs.readJSONSync('./confs/' + process.env.SRV_ID + '.json');
+          const val = fs.readJSONSync(filePath);
           const conf: any = contextInterpretor.updateEnv(val);
 
           resolve(val);
@@ -36,10 +45,10 @@ export class ConfLoader {
           reject(err);
         }
       } else {
-        let tempConf;
+        let tempConf: any;
 
-        if (fs.existsSync('./confs/' + process.env.SRV_ID + '.json')) {
-          tempConf = fs.readJSONSync('./confs/' + process.env.SRV_ID + '.json');
+        if (fs.existsSync(filePath)) {
+          tempConf = fs.readJSONSync(filePath);
 
           if (tempConf.loadConfAfter) {
             ConfLoader.loadConf().catch((err) => {
@@ -47,9 +56,12 @@ export class ConfLoader {
             });
             resolve(tempConf);
           } else {
+            // REALy Weird
+
             ConfLoader.loadConf().then(resolve).catch(reject);
           }
         } else {
+          // REALy Weird
           ConfLoader.loadConf().then(resolve).catch(reject);
         }
       }
@@ -57,10 +69,10 @@ export class ConfLoader {
   }
 
   static loadConf(): Promise<any> {
-    const options: any = {};
-
-    options.url = process.env.CONF_URL + process.env.SRV_ID;
-    options.json = true;
+    const options: request.options = {
+      url: process.env.CONF_URL + process.env.SRV_ID,
+      json: true,
+    };
 
     const secu: UtilsSecu = new UtilsSecu({
       conf: { secretKey: process.env.SECRET, debug: false },
@@ -69,7 +81,11 @@ export class ConfLoader {
     const contextInterpretor: CtxInterpretor = new CtxInterpretor(process.env);
 
     secu.addHeadersKey(options);
+    const filePath = './confs/' + process.env.SRV_ID + '.json';
 
+    /*
+      why do resolve a promise just return request
+    */
     return Promise.resolve(
       request
         .get(options)
@@ -80,16 +96,19 @@ export class ConfLoader {
             data = val.response[0];
 
             fs.ensureDirSync('./confs');
-            fs.writeJSONSync('./confs/' + process.env.SRV_ID + '.json', data, {
+            fs.writeJSONSync(filePath, data, {
               spaces: 2,
             });
           } else {
             if (val && val.code != 200) {
               console.error('online confloader error read JSON', val, options.url);
             }
-            data = fs.readJSONSync('./confs/' + process.env.SRV_ID + '.json');
+            data = fs.readJSONSync(filePath);
           }
 
+          /*
+            conf is never used
+          */
           const conf: any = contextInterpretor.updateEnv(data);
 
           return data;
@@ -98,7 +117,10 @@ export class ConfLoader {
           try {
             console.error('confloader error on JSON ', err);
 
-            const val = fs.readJSONSync('./confs/' + process.env.SRV_ID + '.json');
+            const val = fs.readJSONSync(filePath);
+            /*
+              conf is never used
+            */
             const conf: any = contextInterpretor.updateEnv(val);
 
             return val;
