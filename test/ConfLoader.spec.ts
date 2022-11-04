5cbb7aebbc3030b33a9a92f5c956a3eabd51cb22
Benoît Boutigny <b2devpro@gmail.com>
2022-11-04 08:15:55 +0100

    test: add confLoader tests

--- test/ConfLoader.spec.ts
+++ test/ConfLoader.spec.ts
@@ -9,8 +9,35 @@ describe('ConfLoader', () => {
       expect(new ConfLoader());
     });
 
-    it('should get an error if CONF_URL env is not set', () => {
-      expect(ConfLoader.getConf()).rejects.toThrowError(AssertionError);
+    describe('getConf', () => {
+      it('should get an error if CONF_URL env is not set', () => {
+        expect(ConfLoader.getConf()).rejects.toThrowError(AssertionError);
+      });
+
+      it('should get an error if SRV_ID env is not set', () => {
+        process.env.CONF_URL = 'http://localhost:3114/';
+        expect(ConfLoader.getConf()).rejects.toThrowError(AssertionError);
+      });
+
+      it('should get an error if SECRET env is not set', () => {
+        process.env.CONF_URL = 'http://localhost:3114/';
+        process.env.SRV_ID = 'ezpfiejzfoiehzfouhezofh';
+        expect(ConfLoader.getConf()).rejects.toThrowError(AssertionError);
+      });
+
+      it('should get an error if SECRET env is not set', () => {
+        process.env.CONF_URL = 'http://localhost:3114/';
+        process.env.SRV_ID = 'ezpfiejzfoiehzfouhezofh';
+        process.env.SECRET = 'poipoi';
+        // test fail
+        //expect(ConfLoader.getConf());
+      });
+    });
+
+    describe('loadConf', () => {
+      it('no env var', () => {
+        expect(ConfLoader.loadConf()).rejects.toThrow();
+      });
     });
   });
 });
